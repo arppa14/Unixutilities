94b6ceed13c25260af1b38c43314fb2638816139
Juho <juho.arvio@gmail.com>
2026-03-20 17:44:28 +0200

    Refactor my-grep.c with improved comments
    Updated comments and formatting in my-grep.c

--- my-grep.c
+++ my-grep.c
@@ -2,11 +2,13 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Luetaan syöte rivi kerrrallaan ja tulostetaan osumat
 static void grep_stream(const char *searchterm, FILE *fp) {
     char *line = NULL;
     size_t len = 0;
     ssize_t nread;
 
+    // Kaikkien rivien läpikäynti
     while ((nread = getline(&line, &len, fp)) != -1) {
         if (strstr(line, searchterm) != NULL) {
             printf("%s", line);
@@ -16,6 +18,7 @@ static void grep_stream(const char *searchterm, FILE *fp) {
     free(line);
 }
 
+// Jos ei hakusanaa niin virhe
 int main(int argc, char *argv[]) {
     if (argc < 2) {
         printf("my-grep: hakutermi[tiedosto ...]\n");
@@ -24,11 +27,13 @@ int main(int argc, char *argv[]) {
 
     char *searchterm = argv[1];
 
+    // Jos ei tiedostoja, niin luetaan tandardisyötettä
     if (argc == 2) {
         grep_stream(searchterm, stdin);
         return 0;
     }
 
+    // Virhe jos tiedostoa ei voi avata
     for (int i = 2; i < argc; i++) {
         FILE *fp = fopen(argv[i], "r");
         if (fp == NULL) {
