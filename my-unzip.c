8e0ef3d473c9193ea61c7c1497afe7abf410c691
Juho <juho.arvio@gmail.com>
2026-03-20 17:52:39 +0200

    Update my-unzip.c
    Updated error messages and comments in my-unzip.c.

--- my-unzip.c
+++ my-unzip.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+// Jos ei tiedostoja, niin antaa virheen
 int main(int argc, char *argv[]) {
     if (argc < 2) {
-        printf("my-unzip: tiedosto1 [tiedosto2 ...]\n");
+        printf("my-unzip: file1 [file2 ...]\n");
         exit(1);
     }
 
+    // käydään kaiikki pakatut tiedostot läpi
     for (int i = 1; i < argc; i++) {
         FILE *fp = fopen(argv[i], "r");
         if (fp == NULL) {
-            printf("my-unzip: ei voida avata tiedostoa\n");
+            printf("my-unzip: cannot open file\n");
             exit(1);
         }
+
 
         int count;
         char ch;
 
+        // Luetaan yksi kerrallaan ja tuostetaan myöhemmin X kertaa
         while (fread(&count, sizeof(int), 1, fp) == 1) {
             if (fread(&ch, sizeof(char), 1, fp) != 1) {
                 break;
