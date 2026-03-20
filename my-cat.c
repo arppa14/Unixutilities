fe5018177fefdd080c640fbffe17dfb5a22a7fc3
Juho <juho.arvio@gmail.com>
2026-03-20 17:33:06 +0200

    Update my-cat.c

--- my-cat.c
+++ my-cat.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+
+//Tiedostojen läpikäynti
 int main(int argc, char *argv[]) {
 
     if (argc == 1) {
         return 0;
     }
 
+    //Avataan tiedosto, jotta sen voi lukea
     for (int i = 1; i < argc; i++) {
 
         FILE *fp = fopen(argv[i], "r");
@@ -18,6 +21,7 @@ int main(int argc, char *argv[]) {
 
         char buffer[4096];
 
+        // Tiedoston luku rivi kerrallaan ja tulostus
         while (fgets(buffer, sizeof(buffer), fp) != NULL) {
             printf("%s", buffer);
         }
